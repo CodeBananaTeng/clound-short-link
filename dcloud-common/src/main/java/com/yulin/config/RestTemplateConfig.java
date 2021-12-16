3baa1cd02542e0052a244edd07f01e2bc61aa317
AndyTeng <1153587280@qq.com>
2021-12-16 21:53:39 +0800

    设置

--- dcloud-common/src/main/java/com/yulin/config/RestTemplateConfig.java
+++ dcloud-common/src/main/java/com/yulin/config/RestTemplateConfig.java
@@ -1,8 +1,19 @@
 package com.yulin.config;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,11 +31,44 @@ public class RestTemplateConfig {
     }
 
     @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(5000);
-        factory.setConnectTimeout(5000);
-        return factory;
+    public ClientHttpRequestFactory httpRequestFactory(){
+       return new HttpComponentsClientHttpRequestFactory(httpClient());
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+
+        //设置连接池最大是500个并发
+        connectionManager.setMaxTotal(500);
+
+        //MaxPerRoute是对maxTotal的戏份，每个主机的并发最大是300，route是指域名
+        /**
+         * 类比请求 只请求yulin.net，最大并发300
+         *
+         * 请求   yulin.net，最大并发300
+         * 请求   yulin1.com，最大并发300 但是这个和上面的请求数不能超过500（上面设置的）所以这个只能是200
+         */
+        connectionManager.setDefaultMaxPerRoute(300);
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                //返回数据的超时时间
+                .setSocketTimeout(20000)
+                //连接超时时间
+                .setConnectTimeout(10000)
+                //从连接池中获取连接的超时时间
+                .setConnectionRequestTimeout(1000)
+                .build();
+
+        CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(connectionManager)
+                .build();
+        return closeableHttpClient;
     }
 
 }
