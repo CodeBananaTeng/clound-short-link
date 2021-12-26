94b52d1079fbd4874d9b101fe2c136175f4ba5ab
AndyTeng <1153587280@qq.com>
2021-12-26 17:06:36 +0800

    manager层开发

---
+++ dcloud-link/src/main/java/com/yulin/manage/ShortLinkManager.java
@@ -0,0 +1,34 @@
+package com.yulin.manage;
+
+import com.yulin.model.ShortLinkDO;
+
+/**
+ * @Auther:LinuxTYL
+ * @Date:2021/12/26
+ * @Description:
+ */
+public interface ShortLinkManager {
+
+    /**
+     * 新增短链码
+     * @param shortLinkDO
+     * @return
+     */
+    int addShortLink(ShortLinkDO shortLinkDO);
+
+    /**
+     * 根据短链码寻找短链
+     * @param shortLinkCode
+     * @return
+     */
+    ShortLinkDO findByShortLinkCode(String shortLinkCode);
+
+    /**
+     * 删除
+     * @param shortLinkCode
+     * @param accountNo
+     * @return
+     */
+    int deli(String shortLinkCode ,Long accountNo);
+
+}
---
+++ dcloud-link/src/main/java/com/yulin/manage/impl/ShortLinkManagerImpl.java
@@ -0,0 +1,42 @@
+package com.yulin.manage.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yulin.manage.ShortLinkManager;
+import com.yulin.mapper.ShortLinkMapper;
+import com.yulin.model.ShortLinkDO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther:LinuxTYL
+ * @Date:2021/12/26
+ * @Description:
+ */
+@Component
+@Slf4j
+public class ShortLinkManagerImpl implements ShortLinkManager {
+
+    @Autowired
+    private ShortLinkMapper shortLinkMapper;
+
+    @Override
+    public int addShortLink(ShortLinkDO shortLinkDO) {
+        return shortLinkMapper.insert(shortLinkDO);
+    }
+
+    @Override
+    public ShortLinkDO findByShortLinkCode(String shortLinkCode) {
+        return shortLinkMapper.selectOne(new QueryWrapper<ShortLinkDO>().eq("code",shortLinkCode));
+    }
+
+    @Override
+    public int deli(String shortLinkCode, Long accountNo) {
+
+        ShortLinkDO shortLinkDO = new ShortLinkDO();
+        shortLinkDO.setDel(1);
+
+        int rows = shortLinkMapper.update(shortLinkDO, new QueryWrapper<ShortLinkDO>().eq("code", shortLinkCode).eq("account_no", accountNo));
+        return rows;
+    }
+}
