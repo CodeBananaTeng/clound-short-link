ab1ee0623e03b2eb815e3d2de9fb065984d1a24c
AndyTeng <1153587280@qq.com>
2022-01-15 23:11:08 +0800

    del消息队列配置

--- dcloud-link/src/main/java/com/yulin/service/impl/ShortLinkServiceImpl.java
+++ dcloud-link/src/main/java/com/yulin/service/impl/ShortLinkServiceImpl.java
@@ -215,6 +215,8 @@ public class ShortLinkServiceImpl implements ShortLinkService {
                 .eventMessageType(EventMessageType.SHORT_LINK_DEL.name())
                 .build();
         //TODO
+        rabbitTemplate.convertAndSend(rabbitMQConfig.getShortLinkEventExchange(),rabbitMQConfig.getShortLinkDelRoutingKey(),eventMessage);
+
         return JsonData.buildSuccess();
     }
 
