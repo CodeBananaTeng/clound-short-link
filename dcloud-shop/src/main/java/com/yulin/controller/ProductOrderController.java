cc7509102dcb87c2760a5e51954f7f7b64a4e867
AndyTeng <1153587280@qq.com>
2022-02-17 22:40:03 +0800

    参数形式的防重提交

--- dcloud-shop/src/main/java/com/yulin/controller/ProductOrderController.java
+++ dcloud-shop/src/main/java/com/yulin/controller/ProductOrderController.java
@@ -17,7 +17,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
