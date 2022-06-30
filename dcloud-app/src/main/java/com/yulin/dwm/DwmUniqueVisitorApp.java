15f62f31082de98e503bdd60fb43f7d6b6719724
LinuxTYL <18569760377@163.com>
2022-06-30 22:45:16 +0800

    新增云服务器的环境，修改jdk版本为11

--- dcloud-app/src/main/java/com/yulin/dwm/DwmUniqueVisitorApp.java
+++ dcloud-app/src/main/java/com/yulin/dwm/DwmUniqueVisitorApp.java
@@ -44,6 +44,7 @@ public class DwmUniqueVisitorApp {
             @Override
             public Object getKey(JSONObject jsonObject) throws Exception {
                 return jsonObject.getShort("udid");
+
             }
         });
 
